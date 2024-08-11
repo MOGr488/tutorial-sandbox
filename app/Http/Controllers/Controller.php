06427cc75d32e64bd59cb4431f37146134fe5c10
mogr488 <mogr488@gmail.com>
2024-08-11 23:06:11 +0400

    Update packages Schduel Horizon

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 abstract class Controller
 {
-    //
+    public function index()
+    {
+        $user = new User();
+        $user->name = 'John Doe';
+        $user->email = '';
+    }
 }
