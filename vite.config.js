259b0b71ad28a1e430b8bce651435d7a63678cf2
mogr488 <mogr488@gmail.com>
2024-08-14 12:17:35 +0400

    Fixed Vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host:'0.0.0.0',
+        hmr: {
+            host: 'tutorial.test',
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
     plugins: [
         laravel({
             input: [
